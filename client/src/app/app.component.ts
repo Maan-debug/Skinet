3f8500319e31ef2bb089ae6950bf1ce2f1ee94ed
Maan-debug <maanchauhankiit@gmail.com>
2025-05-14 23:53:39 +0530

    Dotnet and Angular - part 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layout/header/header.component";
+import { HttpBackend, HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  baseUrl = 'https://localhost:5001/api/'
+  private http = inject(HttpClient);
   title = 'Skinet';
+  products: Product[] = [];
+
+  ngOnInit(): void {
+      this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+        next: Response => this.products = Response.data,
+        complete: () => console.log('complete') 
+      })
+  }
 }
